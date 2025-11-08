9af7d22386e04379b501b83a05ddf72ce44134f9
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-11-08 13:15:11 +0300

    Install DRF and create model

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "monitoring",
+    "rest_framework",
+    "drf_yasg",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class StatusCheckResult(models.Model):
+    service_name = models.CharField(max_length=100)
+    success = models.BooleanField()
+    http_status_code = models.PositiveIntegerField(null=True, blank=True)
+    response_time_ms = models.FloatField(null=True, blank=True)
+    checked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        status = "Success" if self.success else "Failed"
+        return f"{self.service_name} - {status} at {self.checked_at.strftime('%Y-%m-%d %H:%M')}"
