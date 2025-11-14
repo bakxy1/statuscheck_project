eb116cacece8eb59d2d85061dba7108a06cc1002
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-11-14 14:59:31 +0300

    add render.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 echo "Applying database migrations..."
 python manage.py migrate
 
+APP_PORT=${PORT:-8000}
+
 if [ "$ENV" = "production" ]; then
     echo "Starting Gunicorn server..."
-    gunicorn config.wsgi:application --bind 0.0.0.0:8000
+    gunicorn config.wsgi:application --bind 0.0.0.0:$APP_PORT
 else
     echo "Starting development server..."
-    python manage.py runserver 0.0.0.0:8000
+    python manage.py runserver 0.0.0.0:$APP_PORT
 fi
